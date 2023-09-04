cfcf8f39e9a514b6696dc837ddbe30b81d63f856
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-04 12:21:21 -0400

    phase-1-arrow-functions/
    phase-1-arrow-functions/

--- phase-1-first-class-functions-master/index.js
+++ phase-1-first-class-functions-master/index.js
@@ -1,156 +1,110 @@
 function Monday() {
-    console.log('Go for a five-mile run");
-    console.log('Pump iron");
+    console.log('Go for a five-mile run');
+    console.log('Pump iron');
 }
 
-function Tuesdat() {
-    console.log("Go for a fiv e-mile run");
-    console.log("Swim 40 laps");
+function Tuesday() {
+    console.log('Go for a five-mile run');
+    console.log('Swim 40 laps');
 }
 
 function Wednesday() {
-   console.log("Go for a five-mile run");
-   console.log("Go for a five-mile run");
-
+   console.log('Go for a five-mile run');
+   console.log('Go for a five-mile run');
 }
 
 function Thursday() {
-    console.log("Go for a five-mile run");
-    console.log("PumP iron");
+    console.log('Go for a five-mile run');
+    console.log('Pump iron');
 }
 
 function Friday() {
-    console.log("Go for a five-mile run");
-    console.log("Swim 40 laps");
-
+    console.log('Go for a five-mile run');
+    console.log('Swim 40 laps');
 }
 
 function runFiveMiles() {
-    console.log("Pump iron");
+    console.log('Pump iron');
 }
 
 function swimFortyLaps() {
-console.log(Swim 40 laps");
-
+    console.log('Swim 40 laps');
 }
 
-function Monday() {
+function exerciseRoutine(postRunActivity) {
     runFiveMiles();
-    liftWeeights();
-
+    postRunActivity();
 }
 
-function excerciseROutine(mostRunActivity) {
-    runFiveMiles();
-    postRunAcitivity();
-
-}
-function Monday() {
-    excerciseROutine(liftWeeights);
-
-}
-
-function excerciseROutine(postRunAcitivity) {
-    excerciseROutine(liftWeeights)(postRunAcitivity)
-}
-    
-
-function exc excerciseROutine(postRunAcitivity)   {
-    runFiveMiles();
-    postRunAcitivity();
-   
-}  
-        
-excerciseROutine(function()
-    console.log)"Stretch! Work that core!);
-});
-
-//"Go for a five-mile run"
-// "Strtech! Wrok that core!"
-
-excerciseROutine(0 +> {
-  console.log("Stretch! Work that core!");
-});
-
-//Or even shorter:
-excerciseROutine(()) => console.log("Stretch ! Work  that core!"));
-
-function morningRoutine(excercise) {
+function morningRoutine(exercise) {
     let breakfast;
 
-    if(excercise === liftWeeights) {
-        hreakfast = "protein bar";
-    } else if (excercise === swimFortyLaps) {
+    if (exercise === liftWeights) {
+        breakfast = "protein bar";
+    } else if (exercise === swimFortyLaps) {
         breakfast = "kale smoothie";
-} else }
- breakfast = "granola";
+    } else {
+        breakfast = "granola";
+    }
+
+    console.log(`Nom nom nom, this ${breakfast} is delicious!`);
 }
-excerciseROutine === afterExcercise(); 
+
+const afterExercise = morningRoutine(liftWeights);
+
+afterExercise();
+
+function returnANamedFunction() {
+    // code goes here
+}
+
+returnANamedFunction;
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("Take no arguments, return an anonymous function!");
+    };
+}
+
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+        console.log("This is a named function!");
+    };
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("This is an anonymous function!");
+    };
+}
 
 //we coulg give this function a name if we wanted to , but since
 // it's only availabel _ inside_ morning(Routine), we don't need to
-\
-    console.log (`Nom nom nom, this ${breakfast} is delicous!);
-  };
-}
 
 
-const afterExcercise = morningRoutine(liftWeights);
 //L0G: Go for a five-mile run
 //L0G: Pump Iron 
 
-afterExcercise;
+////afterExcercise;
 //=> f () {console.log("Nom nom nom, thois $breakkfast} is delicious!`);}
 
 //And we can call the function  later:
-afterExcercise();
+//afterExcercise();
 //L0G: Nom nom nom, this protein bar is delicious !
 
     
-
-function morning(run) {
-    if (run === "fast") {
-        lunch = "salad";
-    }if (run === "fast") {
-      lunch = "salad"; 
-    } else if (run == "pushups") {
-        breakfast = "fruit smoothie";
-    } else {
-    
-    breakfast = "oatmeal";
-   } 
-   
-}
-
-       function returnANamedFunction() {
-      //code goes here 
-
-       }
-         returnANamedFunction;
-
- }
-
   //take a call back function as an atgument call back function it doenst matter what this function returns,long as it callls the call back function
-   
-  function returnsAnAnonymousFunction() {
-       return function() {
-        console.log("take no arguments,return an anonymous function!");
-   };
-
-}
-
-
-          //take no argument 
-
-
-        //The receivesAFunction as an argument
-        /// The receivesAFunction function shouldtake a callback function as an argument call the callback function
-        ///it doesn't matter what this function returns, so long as it calls the callback function
-
- 
-        //the returnsANamedFunction function should: take no arguments ,return a name function
-        //return a nameed function
-        //The returnsAnAnonymousFunction function should: take no arguments , return a name function
-        //The the returnsAnAnonymousFunction function should : take no arguments return an anonymous function 
+ //take no argument 
+//The receivesAFunction as an argument
+/// The receivesAFunction function shouldtake a callback function as an argument call the callback function ///it doesn't matter what this function returns, so long as it calls the callback function
+ //the returnsANamedFunction function should: take no arguments ,return a name function
+ //return a nameed function
+//The returnsAnAnonymousFunction function should: take no arguments , return a name function
+ //The the returnsAnAnonymousFunction function should : take no arguments return an anonymous function 
 
       
+
